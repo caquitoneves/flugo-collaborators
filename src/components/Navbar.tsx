216c46d38a680cacef49dab371df757612c0ce43
Caio Vinicius <caco_neves@outlook.com>
2025-08-07 20:09:39 -0300

    inserção de dropdown no avatar do navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,27 +1,83 @@
-import { Box, Avatar } from "@mui/material";
+import { useState } from "react";
+import {
+  Box,
+  Avatar,
+  Menu,
+  MenuItem,
+  IconButton,
+  Tooltip,
+} from "@mui/material";
+import Person2RoundedIcon from "@mui/icons-material/Person2Rounded";
+import SettingsIcon from "@mui/icons-material/Settings";
+import LogoutIcon from "@mui/icons-material/Logout";
 
-export const Navbar = () => (
-  <Box
-    sx={{
-      width: "100%",
-      height: 72,
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "flex-end",
-      bgcolor: "#fff",
-      borderBottom: "none",
-      px: 4,
-      boxSizing: "border-box",
-    }}
-  >
-    <Avatar
-      src="/avatars/avatar-user.png"
+export const Navbar = () => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const handleOpenMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleCloseMenu = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <Box
       sx={{
-        width: 40,
-        height: 40,
-        border: "2px solid #ECECEC",
+        width: "100%",
+        height: 72,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "flex-end",
         bgcolor: "#fff",
+        px: 4,
+        boxSizing: "border-box",
       }}
-    />
-  </Box>
-);
+    >
+      <Tooltip title="Opções">
+        <IconButton onClick={handleOpenMenu} size="small">
+          <Avatar
+            src="/avatars/avatar-user.png"
+            sx={{
+              width: 40,
+              height: 40,
+              border: "2px solid #ECECEC",
+              bgcolor: "#fff",
+            }}
+          />
+        </IconButton>
+      </Tooltip>
+
+      <Menu
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleCloseMenu}
+        onClick={handleCloseMenu}
+        PaperProps={{
+          elevation: 2,
+          sx: {
+            mt: 1.5,
+            minWidth: 160,
+          },
+        }}
+        transformOrigin={{ horizontal: "right", vertical: "top" }}
+        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+      >
+        <MenuItem sx={{ fontSize: "0.9rem", color: "#A3A3A3" }}>
+          <Person2RoundedIcon fontSize="small" sx={{ mr: 1 }} />
+          Perfil
+        </MenuItem>
+        <MenuItem sx={{ fontSize: "0.9rem", color: "#A3A3A3" }}>
+          <SettingsIcon fontSize="small" sx={{ mr: 1 }} />
+          Configurações
+        </MenuItem>
+        <MenuItem sx={{ fontSize: "0.9rem", color: "#A3A3A3" }}>
+          <LogoutIcon fontSize="small" sx={{ mr: 1 }} />
+          Sair
+        </MenuItem>
+      </Menu>
+    </Box>
+  );
+};
